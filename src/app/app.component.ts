f3e586080de7eef6cca6db5f049dee7e99843045
Diana Carolina Alejo <Diana Carolina Alejo>
2017-01-11 10:25:01 -0500

    Fron dash

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
+declare var google: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app works!';
+
+export class AppComponent implements OnInit{ 
+  map: any
+
+
+ngOnInit(){
+let myLatLng = { lat: 4.598434, lng: -74.076680 };
+	
+		this.map = new google.maps.Map(document.getElementById('map'), {
+			zoom: 12,
+			center: myLatLng,
+			disableDefaultUI: true
+		});
+
+ }
 }
